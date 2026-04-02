9b88444a6743581bcda7a685f1f42cbceca0559d
Emanuel Barbosa <emanuelbarbosa2006@gmail.com>
2026-04-02 23:46:31 +0100

    adicionei data de publicacao nos artigos e botoes de navegacao

--- noticias/models.py
+++ noticias/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 
-from django.db import models
-
 class Artigo(models.Model):
     titulo = models.CharField(max_length=200)
     corpo = models.TextField()
+    data_de_criacao = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.titulo
